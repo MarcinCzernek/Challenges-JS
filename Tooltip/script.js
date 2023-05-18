18063e9454adf1903722499e8bb2b3b2dd556756
MarcinCzernek <mustang.mcz@gmail.com>
2023-05-18 06:40:50 +0200

    Created tooltip

---
+++ Tooltip/script.js
@@ -0,0 +1,10 @@
+const tooltip = document.querySelector('.tooltip');
+
+tooltip.addEventListener('click', function() {
+    if (this.classList.contains('active')) {
+      this.classList.remove('active');
+    } else {
+      this.classList.add('active');
+    }
+    
+  });
